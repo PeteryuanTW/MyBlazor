58b671f72e3c79fc3d5cd2399a3b5b540a01b67d
Peter Yuan <Peter.Yuan@Tm-robot.com>
2022-06-02 18:12:23 +0800

    toast

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Blazored.Toast;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazoredToast();
 
 var app = builder.Build();
 
