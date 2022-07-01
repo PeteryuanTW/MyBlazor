77efaf01324582047a2369cf2e15c040fbce3960
Peter Yuan <Peter.Yuan@Tm-robot.com>
2022-07-01 18:23:59 +0800

    update insert wo

--- Shared/DataClass/SchedulingHistory.cs
+++ Shared/DataClass/SchedulingHistory.cs
@@ -5,15 +5,20 @@
         public DateTime dataTimeID;
         public List<List<Job>> jobs;
         public TimeSpan dueTime;
+        public Dictionary<(string, string), DateTime> machineNextAvailable;
+        public Dictionary<string, DateTime> woNextAvailable;
         public int jobsCount = 0;
 
-        public SchedulingHistory(DateTime dataTimeID, List<List<Job>> jobs, TimeSpan dueTime)
+        public SchedulingHistory(DateTime dataTimeID, List<List<Job>> jobs, TimeSpan dueTime, Dictionary<(string, string), DateTime> machineNextAvailable, Dictionary<string, DateTime> woNextAvailable)
         {
             this.dataTimeID = dataTimeID;
             this.jobs = jobs;
             this.dueTime = dueTime;
+            this.machineNextAvailable = machineNextAvailable;
+            this.woNextAvailable = woNextAvailable;
             SetJobsCount();
         }
+
         private void SetJobsCount()
         {
             foreach (List<Job> jobsList in jobs)
