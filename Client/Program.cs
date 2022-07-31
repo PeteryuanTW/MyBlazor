31a80fff6f754d1dcb3d16f0cd991d3989a2f37f
Wei Hsuan Yuan <peteryuan20@gmail.com>
2022-08-01 02:04:00 +0800

    update ds

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,7 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddDevExpressBlazor();
+//builder.Services.AddDevExpressBlazor();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredToast();
 builder.Services.AddSingleton<StateContainer>();
